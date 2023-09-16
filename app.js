5ea2862b34dc4de2f46e1a0cd22b0f346e7f347f
sharma-varun <varunpost@gmail.com>
2023-09-15 19:15:21 -0500

    add args package

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const getNotes = require("./notes");
 
 console.log(getNotes());
+
+const args = require("args");
+
+args
+  .option("port", "The port on which the app will be running", 3000)
+  .option("reload", "Enable/disable livereloading")
+  .command("serve", "Serve your static site", ["s"]);
+
+const flags = args.parse(process.argv);
+
+console.log(flags);
